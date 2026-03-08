44d69f5cfe78a8f79071d66de068c1e1996b40c8
Gabriel Felipe Freire Silva <gabeielfelipe892@gmail.com>
2026-03-07 22:33:47 -0300

    Class Urgência

---
+++ models/urgencia.py
@@ -0,0 +1,16 @@
+class Urgencia:
+    
+    def urgencia(self, passageiro):
+        """
+        Recebe os dados do passageiro e exibe um alerta crítico na tela,
+        orientando-o a ir direto para o embarque expresso sem pegar fila.
+        """
+        print("\n------------------------------------------")
+        print("ALERTA DE EMBARQUE")
+        print("------------------------------------------")
+        print(f"PASSAGEIRO: {passageiro.nome}")
+        print("STATUS: VOO PRÓXIMO DA PARTIDA")
+        print(">>> ATENÇÃO: DIRIJA-SE IMEDIATAMENTE")
+        print("ao balcão de Embarque Expresso!")
+        print("Não é necessário aguardar senha.")
+        print("------------------------------------------\n")
